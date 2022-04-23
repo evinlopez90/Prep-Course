440bffc8b379f00e4cbbbc9a0e43c0be79a196c2
evin lopez <evinlopezgmail.com>
2022-04-22 19:40:02 -0500

    uno

---
+++ 03-JS-II/homework/expli.concep.txt.js
@@ -0,0 +1,17 @@
+// for
+// Crea un bucle que consiste en tres expresiones opcionales, encerradas
+// en paréntesis y separadas por puntos y comas, seguidas de una 
+// sentencia ejecutada en un bucle.
+// for ([expresion-inicial]; [condicion]; [expresion-final])sentencia
+
+// &&, ||, !
+// AND lógico &&
+// Este operador es similar al AND a nivel de bit pero opera con valores booleanos.
+ //Se utiliza para concatenar comparaciones, es decir, para comprobar varias condiciones . 
+ //El resultado sólo será true si todas las comparaciones o los perandos lo son. Es omo un
+  //producto si uno solo de los factores es 0 (false) el resultado es 0 (false)
+  // OR lógico ||
+//Como el anterior, sirve para realizar comparaciones compuestas y solo devolverá false
+ //cuando todas las comparaciones los sean. Es decir basta que una comparación sea true
+ // para que devuelva el valor false. Es como una suma de ceros y unos, si uno solo de los
+  // sumandos es 1 (true) el resultado es 1(true)
